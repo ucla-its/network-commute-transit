26b63205d1e6dad4b04a536062c044b9d65fd115
David <davidabraham@ucla.edu>
2020-02-26 17:17:44 -0800

    documented .py file

--- .ipynb_checkpoints/networkTransit-checkpoint.py
+++ .ipynb_checkpoints/networkTransit-checkpoint.py
@@ -1,32 +1,9 @@
-'''
-module networkTransit
-Author: David James, 20200226, davidabraham@ucla.edu
-functions:
-    - mp_transitDriver
-    - mp_transitTime
-'''
 import requests
 import pandas as pd
 import multiprocessing as mp
 import datetime as dt
 import logging as lg
 
-'''
-function mp_transitDriver
-A function designed to run mp_transitTime when given a set
-of data to process.
-@param: DataFrame(
-           Series, data['Trip ID']    - int,   Unique ID code for trip
-           Series, data['Source Lat'] - float, source latitude
-           Series, data['Source Lon'] - float, source longiutde
-           Series, data['Dest Lat']   - float, destination latitude
-           Series, data['Dest Lon']   - float, destination longitude
-           Series, data['time']       - str,   time to arrive by the destination
-                                        format can be 15:42:1 or 3.42pm
-           Series, data['date']       - str,   date of the trip
-                                        format can be 11/12/18 or 11-12-18)
-@return: NONE
-'''
 def mp_transitDriver(data):
     lg.basicConfig(format='%(asctime)s %(message)s')
 
@@ -53,21 +30,6 @@ def mp_transitDriver(data):
     now = dt.datetime.now().strftime("%Y%m%d-%H%M")
     df.to_csv(now + 'transitTimes.csv',index=False)
 
-'''
-function mp_transitTime
-A function that makes calls to the OTP server and collects its responses
-@param: tuple(
-          int,index[0] - starting index
-          int,index[1] - ending index)
-@return: DataFrame(
-            Series, 'Trip ID'                    - int,   unique ID for the trip
-            Series, 'Duration (min)'             - int,   duration of the entire trip in minutes
-            Series, 'Walking Time (min)'         - int,   portion of the trip that is walking in minutes
-            Series, 'Transit Time (min)'         - int,   portion of the trip that is on transit in minutes
-            Series, 'Walking Distance (Mi)/ MAX' - float, the walking distance required to/from bus stops
-            Series, 'Transfers'                  - int,   number of transfers taken between transit options
-            Series, 'Message'                    - str,   message saying if a trip was found or not)
-'''
 def mp_transitTime(index):
     # 805, 3220, 8047
     MAXW = 805
